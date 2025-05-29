8ee5f1af54f503c6f953716fd8faab8f0ce2bd40
Vladimir Ghilas <vladghil37@gmail.com>
2025-05-29 23:08:20 +0300

    Update hw_Car.py

--- Lesson01/home_work/hw_Car.py
+++ Lesson01/home_work/hw_Car.py
@@ -1,28 +1,42 @@
 ## Автомобиль
 class Car:
     def __init__(self, capacity, gas_per_km, gas=0):
+        self.gas_per_km = gas_per_km
+        self.capacity = capacity
+        self.mileage = 0
         if gas > capacity:
             self.gas = capacity
-            print("Излишек топлива ...")
+            print(f"Излишек топлива ")
         else:
             self.gas = gas
-        self.capacity = capacity
-        self.gas_per_km = gas_per_km
 
-    def fill(self, fuel) -> None:
+    def fill(self, fuel):
         if self.gas + fuel > self.capacity:
             extra_fuel = self.gas + fuel - self.capacity
             self.gas = self.capacity
-            print(f"Лишний бензин: {extra_fuel}")
+            print(f"Излишек топлива = {extra_fuel}")
         else:
             self.gas += fuel
 
-    def ride(self, distance) -> None:
+    def ride(self, distance):
         max_distance = self.gas / self.gas_per_km
         if distance > max_distance:
+            self.mileage += max_distance
             self.gas = 0
+            print(f"Вы проехали {max_distance} закончился бензин")
+            return max_distance
         else:
             self.gas -= distance * self.gas_per_km
+            self.mileage += distance
+            return distance
 
+    def get_status(self):
+        return f"Топливо: {self.gas}л, Пробег:{self.mileage} км"
 
-# Shift + F6
+car = Car(50, 0.2, 10)
+car.ride(60)
+car.fill(30)
+car.ride(100)
+print(car.get_status())
+
+#Vladimir Ghilas
